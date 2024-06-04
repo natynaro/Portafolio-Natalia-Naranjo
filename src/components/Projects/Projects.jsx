e76efce17e9631cd2e98f4d76359c41cdd8e5e28
natynaro <90767304+natynaro@users.noreply.github.com>
2024-06-03 23:00:51 -0500

    Apariencia

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,7 +8,7 @@ export const Projects = () => {
     <section className={styles.container} id="proyectos">
       <h2 className={styles.title}>Proyectos</h2>
       <p className={styles.description}>
-        Haz clic en la imagen para ver la demo del proyecto y en el botón de GitHub para ver el repositorio del código.
+        Haz clic en la imagen para ver la demo del proyecto y en el botón de Repositorio para ir al link de Github.
       </p>
       <div className={styles.projects}>
         {projects.map((project, id) => (
